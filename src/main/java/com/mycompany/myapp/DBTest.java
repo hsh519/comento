ec0f696f5f0257111725616862a6bb89f5d5380a
hsh_519 <suk000519@naver.com>
2023-01-06 17:05:54 +0900

    코멘토 2주차 과제

---
+++ src/main/java/com/mycompany/myapp/DBTest.java
@@ -0,0 +1,30 @@
+package com.mycompany.myapp;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBTest {
+	
+	String url = "jdbc:mysql://localhost:3306/";
+	String dbName = "testdb";
+	String ID = "root";
+	String PW = "Suk8418!";
+	
+	DBTest() {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			System.out.println("Driver 로딩 성공");
+			Connection conn = DriverManager.getConnection(url+dbName,ID,PW);
+			System.out.println("서버 연결 성공");
+		} catch(ClassNotFoundException e) {
+			System.out.println("Driver 로딩 실패");
+		}  catch(SQLException e) {
+			System.out.println("서버 연결 실패");
+		} 
+	}
+	
+	public static void main(String[] args) {
+		new DBTest();
+	}
+}
